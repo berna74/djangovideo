6ae944b6ebbbb5569ddad8b938b1ffa58c6f5495
Martin Bernatene <msbernatene@gmail.com>
2025-11-04 13:48:23 -0300

    Templates

--- src/django/aulas/urls.py
+++ src/django/aulas/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import hello    
+from .views import hello, goodbye, edad, primer_plantilla, segunda_plantilla , tercera_plantilla  
 
 urlpatterns = [
    path('hello/', hello, name='hello'),  # Placeholder for aulas app URLs
-]
+   path('goodbye/', goodbye, name='goodbye'),  # New URL pattern for goodbye view
+   path('edad/<int:anios>/<int:futuro>', edad, name='edad'),  # New URL pattern for edad view
+   path('plantilla', primer_plantilla, name='primer_plantilla'),  # New URL pattern for primer_plantilla view
+   path('segunda_plantilla', segunda_plantilla, name='segunda_plantilla'),  # New URL pattern for segunda_plantilla view
+   path('tercera_plantilla', tercera_plantilla, name='tercera_plantilla'),  # New URL pattern for tercera_plantilla view
+   ]
--- src/django/aulas/views.py
+++ src/django/aulas/views.py
@@ -1,5 +1,63 @@
 #from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import date
+from django.template import Template, Context
+from django.template.loader import get_template
+from django.shortcuts import render
+
 # Create your views here.
 def hello(request):
     return HttpResponse("Hola mundo desde Django")
+
+def goodbye(request):
+    return HttpResponse("Adiós mundo desde Django")
+
+def edad(request, anios, futuro):
+    incremento = futuro - date.today().year 
+    cumplira = anios + incremento
+    mensaje = "En el año %d tendrás %d años" %(futuro,cumplira)
+
+    return HttpResponse(mensaje)
+
+def primer_plantilla(request):
+    
+    tpl = get_template("primer_plantilla.html")
+
+    ctx = Context(
+        
+    )
+
+    mensaje = tpl.render ({
+            'nombre': 'Juan'
+        })
+
+    return HttpResponse(mensaje)
+
+
+def segunda_plantilla(request):
+    
+    tpl = get_template("segunda_plantilla.html")
+
+    ctx = Context(
+        
+    )
+
+    mensaje = tpl.render ({
+            'nombre': 'Martín',
+            'fecha': date.today()
+        })
+
+    return HttpResponse(mensaje)
+    
+
+def tercera_plantilla(request):
+    
+    tpl = get_template("tercera_plantilla.html")
+
+    
+
+    return render(request, "tercera_plantilla.html", {
+        'nombre': 'Lucía',
+        'fecha': date.today()
+    })
+       
